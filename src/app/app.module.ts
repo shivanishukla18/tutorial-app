57f47ac7ac21ee3c7356580caad32e5242a37598
Shubham Shukla <SShukla@simeiosolutions.com>
2020-04-21 20:51:23 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { StaticpagesModule } from './staticpages/staticpages.module';
+import { CoursesModule } from './courses/courses.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoursesModule,
+    StaticpagesModule,
+    AdminModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
